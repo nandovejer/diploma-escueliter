8170bd7187b1f56a65252150b9926b9d7023d2f2
Nando <johndoe@example.com>
2025-02-10 08:36:11 +0100

    funcionando chrome y firefox

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 function generateImg() {
   const element = document.getElementById("printContent");
+  document.body.classList.add("saving");
+
   html2canvas(element).then((canvas) => {
-    // Convert the canvas to a data URL and open it in a new window
     const dataURL = canvas.toDataURL("image/png");
-    const newWindow = window.open();
-    newWindow.document.write('<img src="' + dataURL + '" />');
+    const downloadImage = document.createElement("a");
+    document.body.appendChild(downloadImage);
+    downloadImage.setAttribute("download", "image");
+    downloadImage.href = dataURL;
+    downloadImage.click();
+    downloadImage.remove();
+    document.body.classList.remove("saving");
   });
+
+  setTimeout(() => {
+    document.body.classList.remove("saving");
+  }, 500);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const btnDownload = document.querySelector("#contentToPdf");
+  const btnDownload = document.querySelector("#contentToImg");
   btnDownload.addEventListener("click", generateImg);
 });
